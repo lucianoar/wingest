3a5791910b836a0ee21a71ef9af6c2cd36c4e305
lucianoar <luciano@littlebox.com.ar>
2014-09-25 18:17:14 -0300

    Grid

---
+++ header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Torneo Wingest</title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <link rel="stylesheet" href="stylesheets/custom.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php require('header.php');?>
+
+<body>
+  <div class="row">
+    <div id="logo" class="small-12">
+      <h1>Torneo Wingest</h1>
+    </div>
+  </div>
+  
+  <div class="row">
+    
+    <div id="resultados" class="sub-section columns small-12 medium-6">
+      <h1>Resultados</h1>
+    </div>
+    
+    <div id="jugadorfecha" class="sub-section columns small-12 medium-6">
+      <h1>Jugador de la fecha</h1>
+    </div>
+  
+  </div>
+
+  <div class="row">
+
+    <div id="calendario" class="sub-section columns small-12 medium-6">
+      <h1>Calendario</h1>
+    </div>
+
+    <div id="goleadores" class="sub-section columns small-12 medium-6">
+      <h1>Goleadores</h1>
+    </div>
+
+  </div>
+  
+  <div class="row">
+
+    <div id="sanciones" class="sub-section columns small-12 medium-3">
+      <h1>Sanciones</h1>
+    </div>
+
+    <div id="credenciales" class="sub-section columns small-12 medium-3">
+      <h1>Credenciales</h1>
+    </div>
+    
+    <div id="videos" class="sub-section columns small-12 medium-3">
+      <h1>Videos</h1>
+    </div>
+    
+    <div id="wingest" class="sub-section columns small-12 medium-3">
+      <h1>Wingest</h1>
+    </div>
+
+  </div>
+
+<!--Footer-->
+<?php require('footer.php');?>
