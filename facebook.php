4d51201854b3d302907208bd81d1d79f934b0fce
lucianoar <luciano@littlebox.com.ar>
2014-10-01 17:25:15 -0300

    facebook test

---
+++ facebook.php
@@ -0,0 +1,25 @@
+<?php
+  error_reporting(-1);
+;0
+  require('facebook-php-sdk-v4-4.0-dev/autoload.php')
+  FacebookSession::setDefaultApplication('779323542128506', 'e31fe0edc86638e42b3457e0344d0096');
+
+?>
+
+<html>
+  <head>
+    <script src="js/facebook.js"></script>
+  </head>
+
+  <body>
+  
+    <div
+      class="fb-like"
+      data-share="true"
+      data-width="450"
+      data-show-faces="true">
+    </div>
+  
+  </body>
+
+</html>
